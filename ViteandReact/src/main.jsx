e6028f8a29721c19ad4755db6c004039bc8a5657
Varad841 <borudevarad2004@gmail.com>
2025-08-06 15:20:22 +0530

    used react library to create an element

--- ViteandReact/src/main.jsx
+++ ViteandReact/src/main.jsx
@@ -1,9 +1,19 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+//Basic creation
+const anotherElement =(
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+//Using react
+const reactElement = React.createElement(
+  'a',
+  {href:'https://google.com',target:'_blank'},
+  'click me to visit google page'
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  reactElement
 )
