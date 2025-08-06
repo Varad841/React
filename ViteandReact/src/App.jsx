201ece12ae9738a596b6e8b7fc5c28ee10aa40bf
Varad841 <borudevarad2004@gmail.com>
2025-08-06 15:13:28 +0530

    Made changes in ViteReact

--- ViteandReact/src/App.jsx
+++ ViteandReact/src/App.jsx
@@ -3,7 +3,9 @@ import Chai from "./chai"
 function App() { 
   return (
     <>
+      <h1>Hello from chai aur react</h1>
        <Chai/>
+       <h4>Bye from chai aur react</h4>
     </>
       
   )
